18e6ece2e92389ebcccbbea960a1e546ef46f870
Michael <maikerurahadian@gmail.com>
2026-05-15 02:16:05 +0700

    refactor: just change the Tittle -> Music-Wails

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "Music-Wails (MW)",
+		Title:  "Music-Wails",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
